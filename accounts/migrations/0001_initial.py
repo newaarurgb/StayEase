891a5473d051b15a501fa1689c09477e8139f92e
codechiefVignesh <vignesharavindh72@gmail.com>
2026-07-19 17:52:53 +0530

    Add missing migration files for accounts and dashboard apps

---
+++ accounts/migrations/0001_initial.py
@@ -0,0 +1,14 @@
+# Generated migration for accounts app
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+    ]
---
+++ dashboard/migrations/0001_initial.py
@@ -0,0 +1,14 @@
+# Generated migration for dashboard app
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+    ]
